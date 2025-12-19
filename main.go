e0c3d5fcf1de2ecbc363ca20254da094b9a345d5
Mariana Werneck <marinhomariana8@gmail.com>
2025-12-18 21:00:15 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	accountOwner := "Fulano"
+	numberAccount := 123456
+	branchNumber := 1234
+	balance := 1000.00
+
+	fmt.Println(accountOwner, numberAccount, branchNumber, balance)
+}
