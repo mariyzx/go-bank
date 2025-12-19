1e5e3356960d35a6f73f7994a12673a5a7426e1c
Mariana Werneck <marinhomariana8@gmail.com>
2025-12-18 21:06:15 -0300

    feat: Add Account struct and update main function to use account instances

--- main.go
+++ main.go
@@ -2,11 +2,28 @@ package main
 
 import "fmt"
 
-func main() {
-	accountOwner := "Fulano"
-	numberAccount := 123456
-	branchNumber := 1234
-	balance := 1000.00
+type Account struct {
+	owner   string
+	number  int
+	branch  int
+	balance float64
+}
 
-	fmt.Println(accountOwner, numberAccount, branchNumber, balance)
+func main() {
+	conta1 := Account{
+		owner:   "Fulano",
+		number:  123456,
+		branch:  1234,
+		balance: 1000.00,
+	}
+
+	conta2 := Account{
+		owner:   "Ciclano",
+		number:  654321,
+		branch:  4321,
+		balance: 2000.00,
+	}
+
+	fmt.Println(conta1)
+	fmt.Println(conta2)
 }
