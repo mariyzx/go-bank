22a7ea4b5b3212e9e128383f9db37159170255f6
Mariana Werneck <marinhomariana8@gmail.com>
2025-12-20 22:14:51 -0300

    feat: Implement Withdraw method and enhance account creation in main function

--- main.go
+++ main.go
@@ -9,21 +9,39 @@ type Account struct {
 	balance float64
 }
 
+func (c *Account) Withdraw(withdrawAmount float64) string {
+	canWithdraw := withdrawAmount > 0 && withdrawAmount <= c.balance
+
+	if canWithdraw {
+		c.balance -= withdrawAmount
+		return "Saque realizado com sucesso"
+	}
+	return "Saldo insuficiente"
+}
+
 func main() {
-	conta1 := Account{
+	// Different ways to create a new account:
+	acc1 := Account{
 		owner:   "Fulano",
 		number:  123456,
 		branch:  1234,
 		balance: 1000.00,
 	}
 
-	conta2 := Account{
-		owner:   "Ciclano",
-		number:  654321,
-		branch:  4321,
-		balance: 2000.00,
-	}
+	var acc2 *Account
+	acc2 = new(Account)
+	acc2.owner = "Ciclano"
+	acc2.number = 654321
+	acc2.branch = 4321
+	acc2.balance = 2000.00
 
-	fmt.Println(conta1)
-	fmt.Println(conta2)
+	acc3 := Account{"Sicrano", 987654, 3219, 3000.00}
+
+	fmt.Println(acc1)
+	fmt.Println(*acc2)
+	fmt.Println(acc3)
+
+	result := acc1.Withdraw(200.0)
+	fmt.Println(result)
+	fmt.Println(acc1.balance)
 }
